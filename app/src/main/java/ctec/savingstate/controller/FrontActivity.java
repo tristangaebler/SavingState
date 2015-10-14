12af2b3494ca0e5be3c2fac6f30e09dab3f99dfe
tristangaebler <tgae6529@740s-pl-115.local>
2015-10-14 09:33:40 -0600

    Added more code to FrontActivty
    Added
    setUpListents
    Getters and Stter

--- app/src/main/java/ctec/savingstate/controller/FrontActivity.java
+++ app/src/main/java/ctec/savingstate/controller/FrontActivity.java
@@ -5,13 +5,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Application;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.CheckBox;
+import ctec.savingstate.model.AndroidSaveState;
+import android.content.Intent;
 
 public class FrontActivity extends Activity {
+
+    private AndroidSaveState saveState;
+    private Button sendToOtherScreenButton;
+    private EditText nameText;
+    private EditText ageText;
+    private CheckBox tiredBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
+
+        saveState = (AndroidSaveState) getApplication();
+        sendToOtherScreenButton = (Button) findViewById(R.id.submitButton);
+        nameText = (EditText) findViewById(R.id.nameText);
+        ageText = (EditText) findViewById(R.id.ageText);
+        tiredBox = (CheckBox) findViewById(R.id.tiredBox);
+
+        setUpListeners();
+    }
+
+    private void setUpListeners() {
+
+        sendToOtherScreenButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View clickView) {
+
+                //Grab user input
+                String name = nameText.getText().toString();
+                int age = Integer.parseInt(ageText.getText().toString());
+                boolean tired = tiredBox.isChecked();
+
+                //Save user input to the saveState
+                saveState.setAge(age);
+                saveState.setUserName(name);
+                saveState.setIsTired(tired);
+
+                //Open new screen
+                Intent newScreenIntent = new Intent(clickView.getContext(), BackpageActivity.class);
+                startActivityForResult(newScreenIntent, 0);
+
+            }
+        });
     }
 
     @Override
